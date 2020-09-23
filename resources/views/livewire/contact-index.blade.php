36f44ce702733652ff0be7af1b89683cf30a994c
zendzo <zendzzo@gmail.com>
2020-09-23 10:40:15 +0700

    artisan make:livewire ContactIndex

---
+++ resources/views/livewire/contact-index.blade.php
@@ -0,0 +1,25 @@
+<div>
+    <table class="table">
+        <thead class="thead-dark">
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Name</th>
+                <th scope="col">Phone</th>
+                <th scope="col"></th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($contacts as $key => $contact)
+                <tr>
+                    <td>{{ $key += 1 }}</td>
+                    <td>{{ $contact->name }}</td>
+                    <td>{{ $contact->phone }}</td>
+                    <td>
+                        <button class="btn btn-sm btn-info text-white">Edit</button>
+                        <button class="btn btn-sm btn-danger text-white">Delete</button>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
